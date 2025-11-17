ca1f30668480fcc9c6110f76ac00512bc38db63a
LucasRoseira <lucas.gomes.roseira@gmail.com>
2025-11-17 11:50:50 -0300

    style: footer responsiviness and add background

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,35 +1,36 @@
-<footer id="footer" class="footer mt-2 p-4">
-    <div class="container">
+<footer id="footer" class="footer p-4">
+    <div class="container-footer">
 
-        <div class="row text-center text-lg-start justify-content-between">
+        <div class="footer-columns">
 
-            <div class="col-12 col-lg-6 mb-2">
+            <div class="footer-block">
                 <h4 class="footer-title fw-bold mb-3">LOREM IPSUM</h4>
+            </div>
 
-                <div class="d-flex flex-column gap-2">
+            <div class="footer-block">
+                <div class="d-flex flex-column gap-2 text-center text-lg-start">
                     <a href="#header" class="fw-bold footer-link">Página inicial</a>
                     <a href="#quem-somos" class="fw-bold footer-link">Quem somos</a>
                     <a href="#produtos" class="fw-bold footer-link">Produtos</a>
                 </div>
             </div>
 
-            <div class="col-12 col-lg-6 mb-2">
-                <h4 class="footer-title fw-bold mb-3 invisible">.</h4>
-
-                <div class="d-flex flex-column gap-2">
+            <div class="footer-block">
+                <div class="d-flex flex-column gap-2 text-center text-lg-start">
                     <a href="#logistica" class="fw-bold footer-link">Logística Reversa</a>
                     <a href="#footer" class="fw-bold footer-link">Contato</a>
                     <a href="#orcamento" class="fw-bold footer-link">Orçamento</a>
                 </div>
             </div>
+
+            <div class="social-box text-center">
+                <p class="fw-bold">
+                    <a href="#" class="footer-link">Instagram</a> ·
+                    <a href="#" class="footer-link">Facebook</a>
+                </p>
+            </div>
         </div>
 
-        <div class="social-box mt-4 p-3 text-center">
-            <p class="fw-bold">
-                <a href="#" class="footer-link">Instagram</a> ·
-                <a href="#" class="footer-link">Facebook</a>
-            </p>
-        </div>
 
     </div>
 </footer>
